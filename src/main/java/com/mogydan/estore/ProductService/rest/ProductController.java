49062a22a0283e30c8598d137b36aa6ad972f461
mogydan <dan3-26@ukr.net>
2022-07-14 20:59:56 +0300

    1. Initial mock controller

---
+++ src/main/java/com/mogydan/estore/ProductService/rest/ProductController.java
@@ -0,0 +1,28 @@
+package com.mogydan.estore.ProductService.rest;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @PostMapping
+    public String createProduct() {
+        return "HTTP Post is handled";
+    }
+
+    @GetMapping
+    public String getProduct() {
+        return "HTTP Get is handled";
+    }
+
+    @PutMapping
+    public String updateProduct() {
+        return "HTTP Put is handled";
+    }
+
+    @DeleteMapping
+    public String deleteProduct() {
+        return "HTTP Delete is handled";
+    }
+}
